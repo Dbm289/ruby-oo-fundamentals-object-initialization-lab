e68ddc35ff2bdff20f0c9802486845bf930f40c6
Dan <DanielMooreRealPerson@gmail.com>
2020-11-23 23:56:24 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+    def initialize(name = "Mutt", breed = "Mutt")
+        @name = name
+        @breed = breed
+     end
+     
+     def name(name)
+        @name = name
+     end
+
+
+     def name
+        @name
+     end
+
+
+
+
+
+end
+
